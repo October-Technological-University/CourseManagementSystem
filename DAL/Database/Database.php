2be54b46e6eb52dbf2ba6ab5a267557bf8fa637d
OsamaR07 <osama.ragab.huissen@gmail.com>
2026-04-17 03:56:50 +0200

    Database and DBContext Setup.

---
+++ DAL/Database/Database.php
@@ -0,0 +1,26 @@
+<?php
+class Database
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $env = parse_ini_file(BASE_PATH . '/config/.env');
+        $this->conn = new mysqli($env['DATABASE_SERVER'], $env['DATABASE_USERNAME'], $env['DATABASE_PASSWORD']);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+
+    public function __destruct()
+    {
+        $this->conn->close();
+    }
+
+}
+?>
