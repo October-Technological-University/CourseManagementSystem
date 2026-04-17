b3ad7a1d654418a3f69523a46e753835464d765f
OsamaR07 <osama.ragab.huissen@gmail.com>
2026-04-17 04:06:20 +0200

    Revert "Support for XAMPP runner and Default PHP runner."
    This reverts commit 38a3fa11e8467652a9e32af3b57af4d2f5997a24.

--- PL/public/index.php
+++ PL/public/index.php
@@ -1,7 +1,4 @@
 <?php
-define('BASE_PATH', __DIR__ . '\\..\\..\\');
-
-require_once BASE_PATH . 'DAL/Database/DBContext.php';
 
 require_once __DIR__ . '/../Controllers/BaseController.php';
 
@@ -18,21 +15,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 // Parse URI and remove base path
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = preg_replace('#/CourseManagementSystem/PL/public/(index(\.php)?)?#', '', $uri);
+$uri = str_replace('/CourseManagementSystem/PL/public', '', $uri);
 $uri = trim($uri, '/');
 $method = $_SERVER['REQUEST_METHOD'];
-
-/* // Ensure database connection is initialized before handling any routes
-DBContext::getInstance();
- */
 
 // ============================================================
 // ROUTER CONFIGURATION
 // Register your routes here in the format:
 // 'METHOD' => ['route/path' => callback]
 // ============================================================
-
-
 
 $router = [
     'GET' => [
@@ -74,8 +65,5 @@ $router = [
 if (isset($router[$method][$uri])) {
     $router[$method][$uri]();
 } else {
-    // For debugging later.
-    // echo '<br>' . $method . $uri . ' not found <br> <br>';
-
     BaseController::error('Route not found', 404);
 }
