df879dad7b32799e9c887b78554d4b8d7fb23511
OsamaR07 <osama.ragab.huissen@gmail.com>
2026-04-19 00:00:52 +0200

    Adding Entities.

--- DAL/Entities/Course.php
+++ DAL/Entities/Course.php
@@ -1,1 +1,128 @@
 <?php
+
+class Course
+{
+    private $id;
+    private $name;
+    private $code;
+    private $description;
+    private $teacher_id;
+    private $capacity;
+    private $start_date;
+    private $end_date;
+    private $CourseImageId;
+    private $created_at;
+
+    public function __construct($name, $code, $teacher_id, $start_date, $end_date, $description = null, $capacity = 30, $CourseImageId = null)
+    {
+        $this->name = $name;
+        $this->code = $code;
+        $this->description = $description;
+        $this->teacher_id = $teacher_id;
+        $this->capacity = $capacity;
+        $this->start_date = $start_date;
+        $this->end_date = $end_date;
+        $this->CourseImageId = $CourseImageId;
+    }
+
+    // Getters
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getCode()
+    {
+        return $this->code;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    public function getTeacherId()
+    {
+        return $this->teacher_id;
+    }
+    public function getCapacity()
+    {
+        return $this->capacity;
+    }
+    public function getStartDate()
+    {
+        return $this->start_date;
+    }
+    public function getEndDate()
+    {
+        return $this->end_date;
+    }
+    public function getCourseImageId()
+    {
+        return $this->CourseImageId;
+    }
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    // Setters
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+    public function setTeacherId($teacher_id)
+    {
+        $this->teacher_id = $teacher_id;
+    }
+    public function setCapacity($capacity)
+    {
+        $this->capacity = $capacity;
+    }
+    public function setStartDate($start_date)
+    {
+        $this->start_date = $start_date;
+    }
+    public function setEndDate($end_date)
+    {
+        $this->end_date = $end_date;
+    }
+    public function setCourseImageId($CourseImageId)
+    {
+        $this->CourseImageId = $CourseImageId;
+    }
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function toArray()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'code' => $this->code,
+            'description' => $this->description,
+            'teacher_id' => $this->teacher_id,
+            'capacity' => $this->capacity,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'CourseImageId' => $this->CourseImageId,
+            'created_at' => $this->created_at
+        ];
+    }
+}
+?>
--- DAL/Entities/User.php
+++ DAL/Entities/User.php
@@ -1,1 +1,113 @@
 <?php
+
+class User
+{
+    private $id;
+    private $email;
+    private $password;
+    private $first_name;
+    private $last_name;
+    private $role;
+    private $profile_picture_id;
+    private $created_at;
+
+    public function __construct($email, $password, $first_name, $last_name, $role = 'student', $profile_picture_id = null)
+    {
+        $this->email = $email;
+        $this->password = $password;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->role = $role;
+        $this->profile_picture_id = $profile_picture_id;
+    }
+
+    // Getters
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+    public function getRole()
+    {
+        return $this->role;
+    }
+    public function getProfilePictureId()
+    {
+        return $this->profile_picture_id;
+    }
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    // Setters
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+    }
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+    public function setProfilePictureId($profile_picture_id)
+    {
+        $this->profile_picture_id = $profile_picture_id;
+    }
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function toArray()
+    {
+        return [
+            'id' => $this->id,
+            'email' => $this->email,
+            'password' => $this->password,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'role' => $this->role,
+            'profile_picture_id' => $this->profile_picture_id,
+            'created_at' => $this->created_at
+        ];
+    }
+
+    public function toArrayWithoutPassword()
+    {
+        $array = $this->toArray();
+        unset($array['password']);
+        return $array;
+    }
+}
+?>
